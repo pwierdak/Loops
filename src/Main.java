d65b91b650b52e4e8a15ece53e4e19c463825b59
pwierdak <paulinawierdak95@gmail.com>
2022-09-20 19:49:28 +0200

    Loops have been added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //For loop
+        String[] cars=new String[]{"Audi", "BMW", "Toyota", "Mercedes"};
+        for (int i=0; i<4; i++){
+            System.out.println(cars[i]);
+        }
+    }
+}
