1f89e567448a90910d20b71d01fca28774068073
pwierdak <paulinawierdak95@gmail.com>
2022-09-20 20:35:13 +0200

    Switch break

---
+++ src/Switch.java
@@ -0,0 +1,14 @@
+public class Switch {
+    public static void main(String[] args){
+        int x=2;
+        switch (x){
+            case 1:
+                System.out.println("random words");
+                break;
+            case 2:
+                System.out.println("words random");
+                break;
+
+        }
+    }
+}
